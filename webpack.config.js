dece13df79d9c3b8d007aae9381c1e4e56b92a39
Baekangsun <ss38318@naver.com>
2022-10-12 13:34:28 +0900

    feat : 폴더 관리를 위한 copyplugin 내용 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output:
   {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'img/[name][ext]'
+    clean: true
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./index.html"
     }),
-    // 플러그인 추가
-    new MiniCssExtractPlugin({
-      filename: "common.css",
-    })
+    new CopyPlugin({
+      patterns: [
+        { from: 'src' }
+      ]
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        // use 배열은 뒤에서부터 적용된다.
-        // use: ["style-loader", "css-loader"]
         use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       // file-loader 세팅
@@ -37,7 +36,6 @@ module.exports = {
     ]
   },
   devServer: {
-    // 개발 서버가 dist 폴더를 제공할 수 있도록 설정
     static: {
       directory: path.resolve(__dirname, 'dist')
     },
