21bdd6ed47bb44616c7d6834cb364aa1782774c2
Baekangsun <ss38318@naver.com>
2022-10-21 18:20:07 +0900

    feat  : 경로 수정 및 style 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  output:
-  {
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./index.html"
+      template: "./index.html",
     }),
     new CopyPlugin({
-      patterns: [
-        { from: 'src' }
-      ]
+      patterns: [{ from: "src" }],
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       // file-loader 세팅
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
         type: "asset/resource",
       },
-    ]
+    ],
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist')
+      directory: path.resolve(__dirname, "dist"),
     },
-    port: 8080
-  }
-}
+    port: 8080,
+  },
+};
